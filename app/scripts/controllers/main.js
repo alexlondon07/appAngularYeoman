26f1e49c2e08f10226dc7c4e073ba1bdf1658769
Alexander Londoño Espejo <alexander.londono@secuencia24.com>
2016-05-31 09:00:24 -0500

    commit first

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appAngularYeomanApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the appAngularYeomanApp
+ */
+angular.module('appAngularYeomanApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
